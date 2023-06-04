964a77ab70e5e079d7b26638d8814c87e01c73c1
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-06-04 17:31:22 +0400

    Update 02-Создание и удаление таблиц.sql

--- 01-Введение в SQL/02-Создание и удаление таблиц.sql
+++ 01-Введение в SQL/02-Создание и удаление таблиц.sql
@@ -14,3 +14,18 @@ CREATE TABLE book
 
 DROP TABLE publisher;
 DROP TABLE book;
+
+
+CREATE TABLE public.book
+(
+    book_id integer NOT NULL,
+    title text NOT NULL,
+    isbn character varying(32) NOT NULL,
+    CONSTRAINT pk_book_id PRIMARY KEY (book_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE IF EXISTS public.book
+    OWNER to postgres;
