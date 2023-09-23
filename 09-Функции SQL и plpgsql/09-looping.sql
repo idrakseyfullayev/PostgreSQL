683cb6e714b3b8d294dc51d5f52282452b03c924
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-23 18:26:56 +0400

    Update 09-looping.sql

--- 09-Функции SQL и plpgsql/09-looping.sql
+++ 09-Функции SQL и plpgsql/09-looping.sql
@@ -1,3 +1,29 @@
+DO $$
+DECLARE
+	counter integer := 0;
+BEGIN
+	WHILE counter <= 10
+		LOOP
+			RAISE NOTICE '%', counter;
+			counter := counter + 1;
+		END LOOP;
+END;
+$$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 CREATE OR REPLACE FUNCTION fibonacci (n INTEGER) 
    RETURNS INTEGER AS $$ 
 DECLARE
