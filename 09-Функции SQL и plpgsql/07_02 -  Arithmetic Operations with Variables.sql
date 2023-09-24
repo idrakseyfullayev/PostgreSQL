2cf97e941ba801106f2ff5604bafb94245232907
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-24 17:15:16 +0400

    Update 07_02 -  Arithmetic Operations with Variables.sql

--- 09-Функции SQL и plpgsql/07_02 -  Arithmetic Operations with Variables.sql
+++ 09-Функции SQL и plpgsql/07_02 -  Arithmetic Operations with Variables.sql
@@ -8,4 +8,17 @@ BEGIN
 	RAISE NOTICE 'value 2: %', y;
 	RAISE NOTICE 'Summer: %', sum_;
 END;	
-$$;		
+$$;
+
+
+DO $$
+DEClARE
+	x int := 20;
+	y int := 15;
+BEGIN
+	
+	RAISE NOTICE 'value 1: %', x;
+	RAISE NOTICE 'value 2: %', y;
+	RAISE NOTICE 'Summer: %', x+y;
+END;	
+$$;
