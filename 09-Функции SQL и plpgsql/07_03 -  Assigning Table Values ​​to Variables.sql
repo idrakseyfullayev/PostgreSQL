d0307f471076e6264aa1bb5150ffc188a0c3ffae
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-24 22:11:32 +0400

    Update 07_03 -  Assigning Table Values ​​to Variables.sql

--- 09-Функции SQL и plpgsql/07_03 -  Assigning Table Values ​​to Variables.sql
+++ 09-Функции SQL и plpgsql/07_03 -  Assigning Table Values ​​to Variables.sql
@@ -6,3 +6,16 @@ BEGIN
 	RAISE NOTICE '%', sum_;
 END;
 $$;
+
+
+DO $$
+DECLARE
+	sum_ int;
+	sum_length int;
+BEGIN
+	sum_ := (SELECT COUNT(*) FROM products);
+	sum_length := (SELECT COUNT(*) FROM products WHERE LENGTH(product_name) > 10);
+	RAISE NOTICE 'product sum: %', sum_;
+	RAISE NOTICE 'Product name length: %', sum_length;
+END;
+$$;
