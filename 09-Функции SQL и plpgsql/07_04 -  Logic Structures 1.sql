7ce488e43bf1e822e3b8010fa0a6eba0083105dc
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-26 16:18:24 +0400

    Update 07_04 -  Logic Structures 1.sql

--- 09-Функции SQL и plpgsql/07_04 -  Logic Structures 1.sql
+++ 09-Функции SQL и plpgsql/07_04 -  Logic Structures 1.sql
@@ -1,1 +1,18 @@
-
+DO $$
+DECLARE
+	test_1 int := 60;
+	test_2 int := 80;
+	test_3 int := 69;
+	avg_ int;
+BEGIN
+	avg_ := (test_1 + test_2 + test_3) / 3;
+	RAISE NOTICE 'Average: %', avg_;
+	
+	IF avg_ >= 70 THEN
+		RAISE NOTICE 'Successful';
+	ELSE
+		RAISE NOTICE 'Unsuccessful';
+	END IF;	
+		
+END;
+$$;
