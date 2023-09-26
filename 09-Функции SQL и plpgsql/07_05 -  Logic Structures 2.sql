58edd465b6d4726f53fdb73a77454fca3cb3f969
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-26 18:31:50 +0400

    Update 07_05 -  Logic Structures 2.sql

--- 09-Функции SQL и plpgsql/07_05 -  Logic Structures 2.sql
+++ 09-Функции SQL и plpgsql/07_05 -  Logic Structures 2.sql
@@ -1,1 +1,12 @@
 
+DO $$
+DECLARE
+	number_ int := 2;
+BEGIN
+	IF number_ % 2 = 0 THEN
+		RAISE NOTICE '% is even number', number_;
+	ELSE
+		RAISE NOTICE '% is odd number', number_;
+	END IF;	
+END;
+$$;
