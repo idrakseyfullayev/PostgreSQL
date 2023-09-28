0c350fab57f7ef862bd99445a9f445d5d1180b4a
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-28 18:15:45 +0400

    Update 07_06 -  While Loop.sql

--- 09-Функции SQL и plpgsql/07_06 -  While Loop.sql
+++ 09-Функции SQL и plpgsql/07_06 -  While Loop.sql
@@ -9,3 +9,19 @@ BEGIN
 	END LOOP;
 END;
 $$;
+
+
+DO $$
+DECLARE
+	counter int := 1;	
+	sum_ int := 0;
+BEGIN
+	WHILE counter <= 10
+	LOOP
+		RAISE NOTICE '%', counter;
+		sum_ := sum_ + counter;
+		counter := counter + 1;
+	END LOOP;
+	RAISE NOTICE 'sum: %', sum_;
+END;
+$$;
