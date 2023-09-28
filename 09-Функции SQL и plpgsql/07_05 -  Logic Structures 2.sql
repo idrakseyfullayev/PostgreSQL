6b95dbd5800019f53b1fdf51ff7ff09ba9e71da4
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-28 12:03:07 +0400

    Update 07_05 -  Logic Structures 2.sql

--- 09-Функции SQL и plpgsql/07_05 -  Logic Structures 2.sql
+++ 09-Функции SQL и plpgsql/07_05 -  Logic Structures 2.sql
@@ -23,3 +23,35 @@ BEGIN
 	END IF;	
 END;
 $$;
+
+
+DO $$
+DECLARE
+	count_ int;
+BEGIN
+	count_ := (SELECT count(*) FROM products WHERE category_id = 4);
+	  
+	IF count_ > 10 THEN
+		RAISE NOTICE 'count % by category', count_;
+	ELSE
+		RAISE NOTICE 'count less than 10';
+	END IF;	
+END;
+$$;
+
+
+DO $$
+DECLARE
+	count_ int;
+	category int;
+BEGIN
+	SELECT count(*), category_id INTO count_, category FROM products
+			  GROUP BY category_id  HAVING category_id = 1;
+			  
+	IF count_ > 10 THEN
+		RAISE NOTICE 'count % by category %', count_, category;
+	ELSE
+		RAISE NOTICE 'count less than 10';
+	END IF;	
+END;
+$$;
