9b5b41007ee344d25d7c9806352cf55460b46330
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-28 22:53:00 +0400

    Update 07_07 -  Loop Usage.sql

--- 09-Функции SQL и plpgsql/07_07 -  Loop Usage.sql
+++ 09-Функции SQL и plpgsql/07_07 -  Loop Usage.sql
@@ -9,3 +9,16 @@ BEGIN
 	END LOOP;
 END;
 $$;
+
+
+DO $$
+DECLARE
+	counter int := 1;
+BEGIN
+	LOOP
+		RAISE NOTICE 'counter: %', counter;
+		EXIT WHEN counter = 15;
+		counter := counter + 1;
+	END LOOP;
+END;
+$$;
