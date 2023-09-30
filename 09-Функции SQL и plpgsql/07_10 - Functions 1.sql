06830a745708e508a5c128d596ff9f9101ee53b0
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-09-30 16:26:53 +0400

    Update 07_10 - Functions 1.sql

--- 09-Функции SQL и plpgsql/07_10 - Functions 1.sql
+++ 09-Функции SQL и plpgsql/07_10 - Functions 1.sql
@@ -1,1 +1,33 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION category_add(cat_id smallint, cat_name varchar(15),
+										 descp text, picture bytea) RETURNS void
+LANGUAGE SQL
+AS $$
+	INSERT INTO categories VALUES (cat_id, cat_name,
+										 descp, picture);
+$$;
+
+SELECT category_add(9::smallint, 'Teafood'::varchar(15), 'Teaweed and fish'::text, '\x'::bytea)
+
+SELECT * FROM categories
