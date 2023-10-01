30cea8b317ff0315e015df3f7322723d8be59955
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-01 14:14:27 +0400

    Update 07_11 - Functions 2.sql

--- 09-Функции SQL и plpgsql/07_11 - Functions 2.sql
+++ 09-Функции SQL и plpgsql/07_11 - Functions 2.sql
@@ -9,3 +9,21 @@ END;
 $$;
 
 SELECT product_name, unit_price,  price_increase(unit_price::float) from products
+
+
+DROP FUNCTION price_increase
+CREATE OR REPLACE FUNCTION price_increase(unit_price float, cat_id smallint ) RETURNS float
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+	IF cat_id > 3 THEN
+		unit_price := unit_price * 0.08 + unit_price;
+	ELSE
+		unit_price := unit_price * 2;
+	END IF;
+	
+	RETURN unit_price;
+END;
+$$;
+
+SELECT product_name, category_id, unit_price,  price_increase(unit_price::float, category_id) from products
