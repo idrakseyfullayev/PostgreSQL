c81b71413a2e4fc87b9158b4e3a75050a9dba14f
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-01 11:17:50 +0400

    Update 07_11 - Functions 2.sql

--- 09-Функции SQL и plpgsql/07_11 - Functions 2.sql
+++ 09-Функции SQL и plpgsql/07_11 - Functions 2.sql
@@ -1,1 +1,11 @@
+DROP FUNCTION price_increase
+CREATE OR REPLACE FUNCTION price_increase(unit_price float) RETURNS float
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+	unit_price := unit_price * 0.08 + unit_price;
+	RETURN unit_price;
+END;
+$$;
 
+SELECT product_name, unit_price,  price_increase(unit_price::float) from products
