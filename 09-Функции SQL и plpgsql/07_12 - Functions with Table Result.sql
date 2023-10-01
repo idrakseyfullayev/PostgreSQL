c1a8149ab1b22348f46e173d48bf1be3072da355
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-01 16:44:07 +0400

    Update 07_12 - Functions with Table Result.sql

--- 09-Функции SQL и plpgsql/07_12 - Functions with Table Result.sql
+++ 09-Функции SQL и plpgsql/07_12 - Functions with Table Result.sql
@@ -1,1 +1,14 @@
+DROP FUNCTION get_product
+CREATE OR REPLACE FUNCTION get_product(pr_name varchar(40)) RETURNS TABLE(
+pr_id smallint, cat_id smallint, prod_name varchar(40)) AS $$
+BEGIN
+	RETURN QUERY
+	SELECT product_id, category_id, product_name
+	FROM products
+	WHERE product_name LIKE INITCAP(pr_name);
+END;
+$$ LANGUAGE PLPGSQL;
 
+SELECT * FROM get_product('c%')
+
+SELECT * FROM products
