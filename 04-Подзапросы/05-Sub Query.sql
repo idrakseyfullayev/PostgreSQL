61971acd1428077740788eb430d7ca744bdbe242
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-07 16:48:05 +0400

    Update 05-Sub Query.sql

--- 04-Подзапросы/05-Sub Query.sql
+++ 04-Подзапросы/05-Sub Query.sql
@@ -3,9 +3,9 @@ SELECT * FROM products WHERE unit_price = (SELECT MAX(unit_price) FROM products)
 
 SELECT * FROM categories;
 SELECT * FROM products WHERE unit_price = (SELECT MAX(unit_price) 
-										   FROM products WHERE category_id = 8);
+					   FROM products WHERE category_id = 8);
 
 SELECT * FROM products WHERE unit_price = (SELECT MAX(unit_price) 
-										   FROM products
-										   JOIN categories USING(category_id)
-										   WHERE category_name = 'Seafood');
+					   FROM products
+					   JOIN categories USING(category_id)
+					   WHERE category_name = 'Seafood');
