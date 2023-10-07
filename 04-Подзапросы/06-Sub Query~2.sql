c35bd0b8af6f23f7972cee453b3f19811d7cbad8
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-07 17:13:04 +0400

    Update 06-Sub Query~2.sql

--- 04-Подзапросы/06-Sub Query~2.sql
+++ 04-Подзапросы/06-Sub Query~2.sql
@@ -1,1 +1,7 @@
-
+SELECT * FROM products
+JOIN categories USING(category_id)
+WHERE category_name = 'Seafood';
+	
+SELECT * FROM products WHERE category_id = (SELECT category_id 
+                      											FROM categories 
+                      											WHERE category_name= 'Seafood');	
