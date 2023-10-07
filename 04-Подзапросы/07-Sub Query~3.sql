7b5795514cd66cc453b8be496d4e2ead35ae681d
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-07 17:25:39 +0400

    Update 07-Sub Query~3.sql

--- 04-Подзапросы/07-Sub Query~3.sql
+++ 04-Подзапросы/07-Sub Query~3.sql
@@ -1,1 +1,6 @@
+SELECT * FROM products WHERE category_id = 8
 
+UPDATE products set unit_price = unit_price + unit_price * 0.1
+						       WHERE category_id = (SELECT category_id
+												    FROM categories
+													WHERE category_name = 'Seafood')
