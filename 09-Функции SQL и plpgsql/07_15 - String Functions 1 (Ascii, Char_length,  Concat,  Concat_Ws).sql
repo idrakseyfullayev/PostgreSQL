a8db1bd37bd6d5e3653ba8dd1d6b64299fbf2d7c
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-07 23:57:33 +0400

    Update 07_15 - String Functions 1 (Ascii, Char_length,  Concat,  Concat_Ws).sql

--- 09-Функции SQL и plpgsql/07_15 - String Functions 1 (Ascii, Char_length,  Concat,  Concat_Ws).sql
+++ 09-Функции SQL и plpgsql/07_15 - String Functions 1 (Ascii, Char_length,  Concat,  Concat_Ws).sql
@@ -1,1 +1,8 @@
+SELECT ASCII('A');
+SELECT ASCII('a');
 
+SELECT CHAR_LENGTH('SQL Tutorial') AS LengthOfString;
+SELECT CHARACTER_LENGTH('SQL Tutorial') AS LengthOfString;
+
+SELECT CONCAT('SQL ' , 'Tutorial ', ' is ', 'fun!');
+SELECT CONCAT_WS(' * ', 'SQL' , 'Tutorial', 'is', 'fun!'); 
