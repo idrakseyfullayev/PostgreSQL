051f9e54c427cdae01997d70351e33e2896f82d5
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-08 17:34:41 +0400

    Update 07_18 - String Functions 3 (REGEXP, REPEAT, REPLACE, REVERSE, RIGHT, RPAD, RTRIM).sql

--- 09-Функции SQL и plpgsql/07_18 - String Functions 3 (REGEXP, REPEAT, REPLACE, REVERSE, RIGHT, RPAD, RTRIM).sql
+++ 09-Функции SQL и plpgsql/07_18 - String Functions 3 (REGEXP, REPEAT, REPLACE, REVERSE, RIGHT, RPAD, RTRIM).sql
@@ -11,12 +11,16 @@ SELECT REPEAT('*', 5);
 
 SELECT REPLACE('ABC', 'B', 'A');
 SELECT REPLACE('Azerbaycan', 'baycan', 'i Yurdu');
+
 SELECT product_name FROM products;
 SELECT REPLACE(product_name, 'C', 'A') FROM products; 
 
 SELECT REVERSE('ABC');
 SELECT REVERSE('Idrak');
 SELECT REVERSE('012345');
+
+SELECT product_name FROM products;
+SELECT REVERSE(product_name) FROM products;
 
 SELECT RIGHT('ABC', 2);
 SELECT RIGHT('ABC', 1);
