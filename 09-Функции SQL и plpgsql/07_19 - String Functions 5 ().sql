7a7571eed004908676740b82286b98e34abfee9a
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-08 17:47:12 +0400

    Update 07_19 - String Functions 5 ().sql

--- 09-Функции SQL и plpgsql/07_19 - String Functions 5 ().sql
+++ 09-Функции SQL и plpgsql/07_19 - String Functions 5 ().sql
@@ -1,1 +1,13 @@
+SELECT SPLIT_PART('2017-12-31','-', 1);
+SELECT SPLIT_PART('2017-12-31','-', 2);
+SELECT SPLIT_PART('2017-12-31','-', 3);
+SELECT SPLIT_PART('Idrak', 'r', 1);
+SELECT SPLIT_PART('Idrak', 'r', 2);
+SELECT SPLIT_PART('Idrak', 'r', 3);
+SELECT SPLIT_PART('Idrak', 't', 1);
+SELECT SPLIT_PART('Idrak', 't', 2);
+SELECT SPLIT_PART('Idrak', 't', 3);
 
+SELECT product_name FROM products;
+SELECT SPLIT_PART(product_name, 'h', 1) FROM products;
+SELECT SPLIT_PART(product_name, 'h', 2) FROM products;
