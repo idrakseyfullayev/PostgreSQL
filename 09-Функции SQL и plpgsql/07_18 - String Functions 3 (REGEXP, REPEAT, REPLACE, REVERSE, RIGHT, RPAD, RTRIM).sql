f7995b3774af02d72c7bc70d95e2c3ffff6f7a62
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-08 17:27:48 +0400

    Update and rename 07_18 - String Functions 3 ().sql to 07_18 - String Functions 3 (REGEXP, REPEAT, REPLACE, REVERSE, RIGHT, RPAD, RTRIM).sql

---
+++ 09-Функции SQL и plpgsql/07_18 - String Functions 3 (REGEXP, REPEAT, REPLACE, REVERSE, RIGHT, RPAD, RTRIM).sql
@@ -0,0 +1,26 @@
+SELECT REGEXP_MATCHES('ABC', '^(A)(..)$', 'g');
+
+SELECT 
+    REGEXP_MATCHES('Learning #Geeksforgeeks #geekPower', 
+         '#([A-Za-z0-9_]+)', 
+        'g');
+
+SELECT REGEXP_REPLACE('John Doe', '(.*) (.*)', '\2, \1');
+
+SELECT REPEAT('*', 5);
+
+SELECT REPLACE('ABC', 'B', 'A');
+SELECT REPLACE('Azerbaycan', 'baycan', 'i Yurdu');
+
+SELECT REVERSE('ABC');
+SELECT REVERSE('Idrak');
+SELECT REVERSE('012345');
+
+SELECT RIGHT('ABC', 2);
+SELECT RIGHT('ABC', 1);
+SELECT RIGHT('Idrak', 4);
+
+SELECT RPAD('ABC', 6, 'xo');
+SELECT RPAD('Azer', 7, 'baycan');
+
+SELECT RTRIM('abcxxzx', 'xyz');
