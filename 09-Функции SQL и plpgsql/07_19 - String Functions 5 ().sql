debd426c8ba6e6eeb89121af0f4c4a7e52c798a4
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-10 11:48:38 +0400

    Update 07_19 - String Functions 5 ().sql

--- 09-Функции SQL и plpgsql/07_19 - String Functions 5 ().sql
+++ 09-Функции SQL и plpgsql/07_19 - String Functions 5 ().sql
@@ -11,3 +11,26 @@ SELECT SPLIT_PART('Idrak', 't', 3);
 SELECT product_name FROM products;
 SELECT SPLIT_PART(product_name, 'h', 1) FROM products;
 SELECT SPLIT_PART(product_name, 'h', 2) FROM products;
+
+SELECT STRPOS('high', 'ig');
+
+SELECT SUBSTRING('ABC', 1, 1);
+SELECT SUBSTRING('Idrak Seyfullayev', 3, 2);
+SELECT SUBSTRING('Seyfullayev', 4, 4);
+
+
+SELECT SUBSTR('alphabet', 3, 1);
+SELECT SUBSTR('alphabet', 3, 2);
+
+SELECT TRIM('ABCCC', 'c');
+SELECT TRIM('ABCCC', 'C');
+SELECT TRIM('ABCCC', 'A');
+SELECT TRIM('ABCCC', 'CA');
+SELECT TRIM('ABCCC', 'AC');
+
+SELECT TRIM('x' FROM 'xTomxx');
+SELECT TRIM(BOTH 'x' FROM 'xTomxx');
+
+SELECT UPPER('hI tHERE');
+SELECT UPPER('hi there');
+SELECT UPPER('HI THERE');
