c4ec8248f51f295fc0a8d50f55cb925072cc5492
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-10 12:17:49 +0400

    Update 07_20 - String Functions 6-Advanced ().sql

--- 09-Функции SQL и plpgsql/07_20 - String Functions 6-Advanced ().sql
+++ 09-Функции SQL и plpgsql/07_20 - String Functions 6-Advanced ().sql
@@ -1,1 +1,25 @@
+SELECT overlay('Txxxxas' placing 'hom' from 2 for 4);
 
+SELECT substring('Thomas' from 2 for 3);
+
+SELECT substring('Thomas' from '...$');
+SELECT substring('Thomas' from '.$');
+SELECT substring('Thomas' from '.....$');
+
+SELECT 	substring('Thomas' from '%#"o_a#"_' for '#');
+
+SELECT REVERSE(SUBSTRING('IDRAK', 3, 3));
+
+SELECT btrim('xyxtrimyyx', 'xy');
+
+SELECT concat('abcde', 2, NULL, 22);
+SELECT concat_ws(',', 'abcde', 2, NULL, 22);
+
+SELECT quote_ident('Foo bar');
+SELECT quote_literal(E'O\'Reilly');
+SELECT quote_literal(42.5);
+SELECT quote_nullable(NULL);
+SELECT quote_nullable(42.5);
+
+SELECT translate('12345', '143', 'axb');
+SELECT translate('12345', '123', 'ax');
