2e00454e4facd84c633eadafe67ee66d53eebdc8
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-10 15:06:07 +0400

    Update and rename 07_21 - Math Functions ().sql to 07_21 - Math Functions (ABS, CBRT, CEIL, DEGREES, DIV, EXP, FACTORIAL, FLOOR).sql

---
+++ 09-Функции SQL и plpgsql/07_21 - Math Functions (ABS, CBRT, CEIL, DEGREES, DIV, EXP, FACTORIAL, FLOOR).sql
@@ -0,0 +1,25 @@
+SELECT ABS(-10); -- Calculate the absolute value of a number
+SELECT ABS(10);
+SELECT abs(-17.4);
+
+SELECT CBRT(8); -- Calculate the cube root of a number
+SELECT cbrt(27.0);
+SELECT cbrt(125);
+
+SELECT CEIL (4.8);
+SELECT CEIL(-12.8);  --	Round a number up to the nearest integer, 
+							    --which is greater than or equal to a specified number
+SELECT 	ceiling(-95.3); -- Same as CEIL
+
+SELECT DEGREES(0.8); -- Convert radians to degrees
+
+SELECT DIV(8,3); -- Return the integer quotient of two numeric values
+SELECT div(9,4); -- integer quotient of y/x
+
+SELECT EXP(1); -- 	exponential
+
+SELECT factorial(5); -- factorial
+
+SELECT FLOOR(10.6); -- nearest integer less than or equal to argument
+SELECT floor(-42.8);
+
