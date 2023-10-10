db8e374704c13151f2b89646a88513d724b5dddd
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-10 16:17:27 +0400

    Update and rename 07_22 - Math Functions ().sql to 07_22 - Math Functions (LN, LOG, MOD, PI, POWER).sql

---
+++ 09-Функции SQL и plpgsql/07_22 - Math Functions (LN, LOG, MOD, PI, POWER).sql
@@ -0,0 +1,16 @@
+SELECT LN(3); --Return the natural logarithm of a numeric value
+SELECT ln(2.0);
+
+SELECT LOG(1000); -- Return the base 10 logarithms of a numeric value
+SELECT log(100.0);
+
+SELECT LOG(2, 64); --Return the logarithm of a numeric value to a specified base
+
+SELECT MOD(10,4);--Divide the first parameter by the second one and return the remainder
+SELECT mod(9,4);
+
+SELECT PI(); --"π" constant
+SELECT 	pi();
+
+SELECT POWER(5, 3); -- Raise a numeric value to the power of a second numeric value
+SELECT POWER(5, 2);
