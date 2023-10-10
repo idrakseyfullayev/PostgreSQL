9e90199b5f872eb82711b95e524e850179fbd79d
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-10 16:44:04 +0400

    Update 07_23 - Math Functions ().sql

--- 09-Функции SQL и plpgsql/07_23 - Math Functions ().sql
+++ 09-Функции SQL и plpgsql/07_23 - Math Functions ().sql
@@ -1,1 +1,27 @@
+SELECT RADIANS(60); -- Convert degrees to radians
+
+SELECT RANDOM(); -- Return a random number that ranges from 0 to 1
+
+SELECT ROUND(10.8); -- round to nearest integer
+SELECT ROUND(10.4);
+SELECT round(42.4382, 3); -- round to decimal places
+
+SELECT SCALE(1.234); --	Return the number of decimal digits in the fractional part
+SELECT SCALE(1.12);
+SELECT SCALE(1.1);
+SELECT SCALE(1);
+
+SELECT SIGN(-1); -- Return the sign (positive, negative) of a numeric value
+SELECT SIGN(-7.8);
+SELECT SIGN(9.8);
+SELECT SIGN(0);
+
+SELECT SQRT(3.0); -- Return the square root of a numeric value
+SELECT sqrt(2.0);
+SELECT SQRT(9);
+SELECT SQRT(81);
+SELECT SQRT(0);
+
+
+
 
