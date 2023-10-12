8209ad5ff0c05931aaa0bfe6b14b686f4343b321
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-12 10:05:49 +0400

    Update 07_17 - String Functions 3 (LOWER, LPAD, LTRIM, MD5, POSITION).sql

--- 09-Функции SQL и plpgsql/07_17 - String Functions 3 (LOWER, LPAD, LTRIM, MD5, POSITION).sql
+++ 09-Функции SQL и plpgsql/07_17 - String Functions 3 (LOWER, LPAD, LTRIM, MD5, POSITION).sql
@@ -1,13 +1,13 @@
-SELECT LOWER('hI tHeRe');
+SELECT LOWER('hI tHeRe'); -- Convert a string to lowercase
 SELECT LOWER('HI THERE');
 SELECT LOWER('TOM');
 
-SELECT LPAD('7235', 8, '20');
+SELECT LPAD('7235', 8, '20'); -- Pad on the left a a string with a character to a certain length
 
-SELECT LTRIM('00123', '001');
+SELECT LTRIM('00123', '001'); -- 	Remove the longest string that contains specified characters from the left of the input string
 
-SELECT MD5('ABC')
+SELECT MD5('ABC'); -- Return MD5 hash of a string in hexadecimal
 
-SELECT POSITION('Tutorial' IN 'GeeksForGeeks Tutorial');
+SELECT POSITION('Tutorial' IN 'GeeksForGeeks Tutorial'); -- Return the location of a substring in a string
 SELECT POSITION('B' in 'A B C')
 SELECT POSITION('ak' IN 'Idrak')
