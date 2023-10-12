ec2ffff3919936f75a5bd064a7cf65ca23b8b246
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-12 10:06:04 +0400

    Update 07_17 - String Functions 3 (LOWER, LPAD, LTRIM, MD5, POSITION).sql

--- 09-Функции SQL и plpgsql/07_17 - String Functions 3 (LOWER, LPAD, LTRIM, MD5, POSITION).sql
+++ 09-Функции SQL и plpgsql/07_17 - String Functions 3 (LOWER, LPAD, LTRIM, MD5, POSITION).sql
@@ -4,7 +4,7 @@ SELECT LOWER('TOM');
 
 SELECT LPAD('7235', 8, '20'); -- Pad on the left a a string with a character to a certain length
 
-SELECT LTRIM('00123', '001'); -- 	Remove the longest string that contains specified characters from the left of the input string
+SELECT LTRIM('00123', '001'); -- Remove the longest string that contains specified characters from the left of the input string
 
 SELECT MD5('ABC'); -- Return MD5 hash of a string in hexadecimal
 
