62cefeca6134c06105eea10117ac866064e3c7b7
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-12 10:01:01 +0400

    Update 07_16 - String Functions 2 (FORMAT, INITCAP, LEFT, LENGTH).sql

--- 09-Функции SQL и plpgsql/07_16 - String Functions 2 (FORMAT, INITCAP, LEFT, LENGTH).sql
+++ 09-Функции SQL и plpgsql/07_16 - String Functions 2 (FORMAT, INITCAP, LEFT, LENGTH).sql
@@ -1,11 +1,11 @@
-SELECT FORMAT('Hello %s %s','PostgreSQL', 12);
+SELECT FORMAT('Hello %s %s','PostgreSQL', 12); -- 	Format arguments based on a format string
 SELECT FORMAT('Hello %s, %1$s, %1$s', 'World');
 SELECT FORMAT('Hello %s, %1$s, %s, %2$s', 'World', 'sql');
 
-SELECT INITCAP('hI tHERE');
+SELECT INITCAP('hI tHERE'); -- 	Convert words in a string to title case
 SELECT INITCAP('hi there');
 SELECT INITCAP('hI tHeRe');
 
-SELECT LEFT('Salam', 3);
+SELECT LEFT('Salam', 3); -- 	Return the first n character in a string
 
-SELECT LENGTH('Salam')
+SELECT LENGTH('Salam'); -- Return the number of characters in a string
