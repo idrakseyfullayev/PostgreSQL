99e4b3eda1e1c2690b504f5d8b6f7167450d004f
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-12 09:56:02 +0400

    Update 07_15 - String Functions 1 (Ascii, CHR, Char_length,  Concat,  Concat_Ws).sql

--- 09-Функции SQL и plpgsql/07_15 - String Functions 1 (Ascii, CHR, Char_length,  Concat,  Concat_Ws).sql
+++ 09-Функции SQL и plpgsql/07_15 - String Functions 1 (Ascii, CHR, Char_length,  Concat,  Concat_Ws).sql
@@ -1,13 +1,13 @@
 SELECT ASCII('A'); -- Return the ASCII code value of a character or Unicode code point of a UTF8 character
 SELECT ASCII('a');
 
-SELECT CHR(65);
-SELECT CHAR_LENGTH('SQL Tutorial') AS LengthOfString;
+SELECT CHR(65); -- Convert an ASCII code to a character or a Unicode code point to a UTF8 character
+SELECT CHAR_LENGTH('SQL Tutorial') AS LengthOfString; -- Return the length of the string:
 SELECT CHARACTER_LENGTH('SQL Tutorial') AS LengthOfString;
 
-SELECT CONCAT('SQL ' , 'Tutorial ', ' is ', 'fun!');
-SELECT CONCAT_WS(' * ', 'SQL' , 'Tutorial', 'is', 'fun!');
-SELECT CONCAT ('Post', 'greSQL')
-SELECT CONCAT ('Post', ' ', 'greSQL')
-SELECT 'Post' || 'greSQL'
-SELECT 'Post' || ' ' || 'greSQL'
+SELECT CONCAT('SQL ' , 'Tutorial ', ' is ', 'fun!'); -- Concatenate two or more strings into one
+SELECT CONCAT_WS(' * ', 'SQL' , 'Tutorial', 'is', 'fun!'); -- Concatenate strings with a separator
+SELECT CONCAT ('Post', 'greSQL');
+SELECT CONCAT ('Post', ' ', 'greSQL');
+SELECT 'Post' || 'greSQL'; -- Concatenate two or more strings into one
+SELECT 'Post' || ' ' || 'greSQL';
