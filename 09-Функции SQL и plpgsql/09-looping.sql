3c5fbd1a8f701d41d519bc948c12dc66bd08339f
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-22 00:17:51 +0400

    Update 09-looping.sql

--- 09-Функции SQL и plpgsql/09-looping.sql
+++ 09-Функции SQL и plpgsql/09-looping.sql
@@ -42,6 +42,31 @@ BEGIN
 END $$ LANGUAGE PLPGSQL;
 
 
+DO $$
+DECLARE
+	counter int := 0;
+	i int := 0;
+	j int := 1;
+	n int := 8;
+BEGIN
+	if n = 1 THEN
+		RAISE NOTICE '0';
+	END IF;
+		
+	RAISE NOTICE '%', i;
+	RAISE NOTICE '%', j;	
+
+	LOOP
+		
+		SELECT j, i +j INTO i, j;
+		counter = counter + 1;
+		RAISE NOTICE '%', j;
+		EXIT WHEN counter = n-2;
+	END LOOP;	
+END
+$$;
+
+
 CREATE OR REPLACE FUNCTION fibonacci (n INTEGER) 
    RETURNS INTEGER AS $$ 
 DECLARE
