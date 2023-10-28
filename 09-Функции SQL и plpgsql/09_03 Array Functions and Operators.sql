8526d1996018f342816e4995155e6b448dc75fd3
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-28 15:56:57 +0400

    Update 09_03 Array Functions and Operators.sql

--- 09-Функции SQL и plpgsql/09_03 Array Functions and Operators.sql
+++ 09-Функции SQL и plpgsql/09_03 Array Functions and Operators.sql
@@ -1,1 +1,7 @@
+SELECT ARRAY[1,2,3];
+SELECT ARRAY(SELECT product_id FROM products);
+SELECT ARRAY(SELECT product_name FROM products);
 
+SELECT array_append(ARRAY[]::int[], 1);
+
+SELECT array_cat(ARRAY[10]::smallint[], ARRAY(SELECT product_id FROM products));
