3d7eef9ff83a25a96ed589e8196c7f5708a61fea
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-10-28 15:29:18 +0400

    Update 09_02 Function exercises.sql

--- 09-Функции SQL и plpgsql/09_02 Function exercises.sql
+++ 09-Функции SQL и plpgsql/09_02 Function exercises.sql
@@ -1,1 +1,13 @@
 
+CREATE OR REPLACE FUNCTION backup_customers() RETURNS void AS $$
+	DROP TABLE IF EXISTS backedup_customers;
+	
+	CREATE TABLE backedup_customers AS
+	SELECT * FROM customers;
+-- 	SELECT INTO backedup_customers
+-- 	FROM customers;
+$$ LANGUAGE SQL;
+
+SELECT backup_customers();
+
+SELECT COUNT(*) FROM backedup_customers;
