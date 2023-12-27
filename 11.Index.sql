9f41ecf23c1d0c7e1b8d1a8a661d193a2efc3834
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-12-27 11:05:52 +0400

    Update 11.Index.sql

--- 11.Index.sql
+++ 11.Index.sql
@@ -1,1 +1,28 @@
 
+CREATE TABLE perf_test
+(
+	id int,
+	reason text COLLATE "C",
+	annotation text COLLATE "C"
+);
+
+INSERT INTO perf_test(id, reason, annotation)
+SELECT s.id, md5(random()::text), null
+FROM generate_series(1, 10000000) AS s(id)
+ORDER BY random();
+
+UPDATE perf_test
+SET annotation = md5(UPPER(random()::text));
+
+
+SELECT * FROM perf_test
+LIMIT 10;
+
+SELECT * 
+FROM perf_test
+WHERE id = 3700000;
+
+EXPLAIN
+SELECT * 
+FROM perf_test
+WHERE id = 3700000;
