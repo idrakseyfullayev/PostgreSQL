89cee3aef80754d273e7836ff942f3626c1a2400
idrakseyfullayev <127197751+idrakseyfullayev@users.noreply.github.com>
2023-12-27 20:38:30 +0400

    Update 11.Index.sql

--- 11.Index.sql
+++ 11.Index.sql
@@ -106,3 +106,21 @@ SELECT *
 FROM perf_test
 WHERE annotation LIKE 'AB%';
 
+*******************************************************************************
+
+EXPLAIN
+SELECT * 
+FROM perf_test
+WHERE LOWER(annotation) LIKE 'ab%';
+
+CREATE INDEX idx_perf_test_annotation_lower ON perf_test(LOWER(annotation));
+
+EXPLAIN
+SELECT * 
+FROM perf_test
+WHERE LOWER(annotation) LIKE 'ab%';
+
+SELECT * 
+FROM perf_test
+WHERE LOWER(annotation) LIKE 'ab%';
+
